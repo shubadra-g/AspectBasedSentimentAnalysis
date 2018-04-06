37a75f7646c31883e7c3a0b15cb2c02bbdbf5177
Mohit Adwani <mohitadwani@gmail.com>
2018-04-06 16:32:59 -0500

    added something

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -11,23 +11,23 @@ import string
 def get_document(fname):
     f = open(fname, "r")
     data = f.read()
-    print(data)
     f.close()
     return data
 
-fname = 'data 1_train.csv'    
+fname = 'project_2_train/' + 'data 1_train.csv'
 f = get_document(fname)
-tokens = f.split(',')
-# remove punctuations
-table = str.maketrans('', '', string.punctuation) 
-tokens = [w.translate(table) for w in tokens]
-# remove tokens that are not alphbetic
-tokens = [word for word in tokens if word.isalpha()]
-
-# remove stop words
-stop_words = set(stopwords.words('english'))
-tokens = [w for w in tokens if not w in stop_words]
-# filter out short tokens
-tokens = [word for word in tokens if len(word) > 1]
+tokens = f.split(',') # columns
 print(tokens)
-    
+# remove punctuations
+# table = str.maketrans('', '', string.punctuation)
+# print(table)
+# tokens = [w.translate(table) for w in tokens]
+# # remove tokens that are not alphbetic
+# tokens = [word for word in tokens if word.isalpha()]
+#
+# # remove stop words
+# stop_words = set(stopwords.words('english'))
+# tokens = [w for w in tokens if not w in stop_words]
+# # filter out short tokens
+# tokens = [word for word in tokens if len(word) > 1]
+# print(tokens)
