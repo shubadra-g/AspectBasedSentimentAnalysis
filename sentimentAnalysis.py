4b25ab16339fd07e2fd93bf8bb99aeae5401144c
Mohit Adwani <mohitadwani@gmail.com>
2018-04-10 22:25:28 -0500

    aspect term location found. Success. to assign the weight to every word now

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -7,11 +7,12 @@ Created on Fri Apr  6 16:09:25 2018
 
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import word_tokenize
 import string
 import re
 
 find = lambda searchList, elem: [[i for i, x in enumerate(searchList) if x == e] for e in elem]
-fname = 'project_2_train/' + 'dara 1_train.csv'
+fname = 'project_2_train/' + 'data 2_train.csv'
 
 stopwords_set = set(stopwords.words('english'))
 stopwords_set = {'i', 'shan', 'just', 'how', 'each', 'out', 'themselves', 'their', 'before', 'were', 'very', 'as', 'further', 'his', 'a', 'once', 'youve', 'y', 'is', 'shouldve', 'youll', 'on', 'd', 'm', 'under', 'haven', 'which', 'only', 'them', 'was', 'by', 'needn', 'whom', 'that', 'when', 's', 'isn', 'its', 'no', 'wasn', 'in', 'we', 'theirs', 'those', 'this', 'having', 'and', 'ain', 'most', 'up', 'off', 'being', 'aren', 'shouldn', 'ourselves', 'from', 'down', 'herself', 'her', 'you', 'are', 'its', 'who', 'the', 'here', 'where', 'your', 'youd', 'she', 'didn', 'weren', 'about', 'has', 'our', 'an', 'yourselves', 'or', 'hasn', 'again', 'while', 'does', 'him', 'shes', 'above', 'below', 'itself', 'to', 'through', 'will', 'couldn', 'hers', 'they', 'doing', 'because', 'he', 'what', 'such', 'youre', 'nor', 'too', 'should', 'ours', 'then', 'himself', 'all', 'of', 'mightn', 'between', 'now', 'against', 'some', 'with', 'until', 'am', 'other', 'at', 'can', 'over', 'mustn', 'wouldn', 'do', 'for', 'after', 'hadn', 'me', 'been', 'same', 'doesn', 'my', 'these', 'll', 'did', 'had', 'it', 'so', 'ma', 'during', 'than', 'o', 'yourself', 'own', 'have', 're', 've', 'be', 'why', 't', 'there', 'more', 'won', 'yours', 'few', 'into', 'thatll', 'any', 'myself', 'both', 'don', 'if'}
@@ -22,63 +23,97 @@ stopwords_set = {'i', 'shan', 'just', 'how', 'each', 'out', 'themselves', 'their
 f = open(fname, 'r')
 for i, line in enumerate(f):
     if i != 0:
+        ''' Splitting the columns based on comma - since it is csv'''
         columns = line.split(',')
-        columns[1] = columns[1].replace('[comma]', '')
-        columns[2] = columns[2].replace('[comma]', '')
+
+        ''' The comma in the actual sentence was represented as [commma] because of csv format, replace that '''
+        columns[1] = columns[1].replace('[comma]', ',')
+        columns[2] = columns[2].replace('[comma]', ',')
+
+        ''' Some aspect terms are mic in the sentence where computer/mic is present '''
+        columns[1] = columns[1].replace('-', ' ')
+        columns[2] = columns[2].replace('-', ' ')
+        columns[1] = columns[1].replace('/', ' ')
+        columns[2] = columns[2].replace('/', ' ')
+
+        ''' NOt used anymore'''
         tokenizer = RegexpTokenizer(r'\w+') # doesn't work if the special char is in the token
-        columns[1] = tokenizer.tokenize(columns[1].lower())
-        columns[2] = tokenizer.tokenize(columns[2].lower())
-        columns[3] = []
+        # columns[2] = tokenizer.tokenize(columns[2].lower())
+
+        '''Tokenize the words'''
+        columns[1] = word_tokenize(columns[1])
+        columns[2] = word_tokenize(columns[2])
+
+        for j, elem in enumerate(columns[1]):
+            '''Remove special characters'''
+            columns[1][j] = re.sub('[^0-9a-zA-Z]+', '', elem).lower()
+            ''' didn't is tokenized as did n't hence following code '''
+            if columns[1][j] == 'nt':
+                columns[1][j] = 'not'
+
+        for j, elem in enumerate(columns[2]):
+            '''Remove special characters'''
+            columns[2][j] = re.sub('[^0-9a-zA-Z]+', '', elem).lower()
+            ''' didn't is tokenized as did n't hence following code '''
+            if columns[2][j] == 'nt':
+                columns[2][j] = 'not'
+
+        '''Remove empty string tokens'''
+        columns[1] = [x for x in columns[1] if x != '']
+        columns[2] = [x for x in columns[2] if x != '']
+
+        ''' Remove stop words '''
+        columns[1] = [word for word in columns[1] if word not in stopwords_set]
         columns[1] = [word for word in columns[1] if word not in stopwords_set]
         columns[2] = [word for word in columns[2] if word not in stopwords_set]
 
         columns[1] = ' '.join(columns[1])
         columns[2] = ' '.join(columns[2])
 
-        # columns[3] = columns[1].count(columns[2])
+        ''' The aspect term location given is not proper - hence extracting the location by ourselves '''
+        columns[3] = []
+        ''' Finds the location of the aspect term in the sentence '''
         for m in re.finditer(columns[2], columns[1]):
             columns[3].append([m.start(), m.end()])
-        # print(columns)
+            ''' since some aspect terms are not surrounded by spaces, hence below code '''
+            if m.start() != 0 and columns[1][m.start() - 1] != ' ':
+                columns[1] = columns[1][:m.start()] + ' ' + columns[1][m.start():]
+
+        ''' Because of adding space in above code, the position is messed up, hence redoing it. '''
+        columns[3] = []
+
+        for m in re.finditer(columns[2], columns[1]):
+            columns[3].append([m.start(), m.end()])
+
+        ''' Tokenizing the words again '''
         columns[1] = columns[1].split(' ')
         columns[2] = columns[2].split(' ')
+        ''' New column to specify the aspect term location in the list '''
         columns.append([])
         # columns[5] = []
+        ''' for multiple positions of the columns '''
         for elem in columns[3]:
             if elem[0] == 0: #start index of the 1st occurance of the aspect term
                 columns[5].append([0])
             else:
-                temp_len = elem[0]
+                temp_len = elem[0] # assign the start position of the aspect term
                 for j, tokens in enumerate(columns[1]):
-                    temp_len -= (len(tokens) + 1) # +1 for blank space
-                    if temp_len == 0:
+                    temp_len -= (len(tokens) + 1) # Counting the words till the position - +1 for blank space
+                    if temp_len == 0: # Reached the aspect word
                         columns[5].append([j+1])
                         break
-            # for j, elem2 in enumerate(columns[2]): #aspect term column
-            #     if j > 0:
-            #         columns[5][-1].append(columns[5][-1][-1]+1)
-            for k in range(len(columns[2])-1):
+            for k in range(len(columns[2])-1): # if multiple words in the aspect term - tag the following words in the sentence
                 columns[5][-1].append(columns[5][-1][-1]+1)
-        if len(columns[3]) > 1:
-            # print(columns[1],"      ", columns[2], "    ", columns[3])
+
+        ''' if not found the aspect term location in the list then do '''
+        if len(columns[5]) < 1:
             print(line)
             print(columns)
-            # pass
+            pass
 
-        # columns[3] = find(columns[1], columns[2])
-        # for elem in columns[3]:
-        #     if len(elem) > 1:
-        #         # print(columns[3])
-        #         pass
-
-        # print(columns[1])
-        # print(columns[1].index(columns[2][0]))
-        # print(columns[1])
-        # word_tokens = word_tokenize(columns[1])
-
-        # print(columns)
-        if i == 4:
-            # pass
+        if i == 10:
             break
+            # pass
 
 # tokens = f.split(',') # columns
 # remove punctuations
